66033859f84dbacae3e5687e0e713484439dac4d
Axel <axelfernandezcurros@gmail.com>
2020-09-28 15:24:41 +0200

    changes

--- bintodec.py
+++ bintodec.py
@@ -1,3 +1,25 @@
-binary = input("Enter number in Binary Format: ")
-decimal = int(binary, 2)
-print(decimal)
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys
+def btd(decimal):
+    try:
+        decimal = int(sys.argv[1], 2)
+        print(decimal)
+    except ValueError:
+        print("ERROR: bintodec needs binary characters")
+        exit(1)
+
+try:
+    if not sys.stdin.isatty():
+        y = sys.stdin.readlines()[0].split(" ")[0].rstrip("\n").lstrip("\n")
+        btd(y)
+        exit(0)
+except ValueError:
+    print("ERROR: bintodec needs binary characters")
+    exit(1)
+
+if len(sys.argv) == 1:
+    print("ERROR: bintodec needs one argument")
+    exit(2)
+
+btd(sys.argv[1])
