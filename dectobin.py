2dcbaab506c0384cb94d6bac70129e806dd5449a
Axel <axelfernandezcurros@gmail.com>
2020-09-28 15:50:47 +0200

    changes

--- dectobin.py
+++ dectobin.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import sys
-def btd(binary):
+def dtb(decimal):
     try:
-        binary = int(sys.argv[1], 2)
-        print(binary)
+        return bin(int(decimal)).replace("0b", "")
     except ValueError:
-        print("ERROR: bintodec needs binary characters")
+        print("ERROR: dectobin needs decimal characters")
         exit(1)
 try:
     if not sys.stdin.isatty():
         y = sys.stdin.readlines()[0].split(" ")[0].rstrip("\n").lstrip("\n")
-        btd(y)
+        dtb(y)
         exit(0)
 except ValueError:
-    print("ERROR: bintodec needs binary characters")
+    print("ERROR: dectobin needs decimal characters")
     exit(1)
 
 if len(sys.argv) == 1:
-    print("ERROR: bintodec needs one argument")
+    print("ERROR: dectobin needs only one argument")
     exit(2)
 
-btd(sys.argv[1])
+print(dtb(sys.argv[1]))
