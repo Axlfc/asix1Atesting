721a446634c9346f0f45c7c816e3de944637135f
axel <axelfernandezcurros@gmail.com>
2020-09-30 20:25:11 +0200

    changes

--- aliases/.tmux_path.sh
+++ aliases/.tmux_path.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
 #must make this file executable 'chmod +x ~/.tmux_path.sh'
-precmd () 
-{
-  if [ -n "$TMUX" ]; then
-    tmux set-window-option -q window-status-format "#[fg=cyan bg=cyan] | #[fg=white, bg=cyan] #I | ${PWD##/*/} #[fg=cyan, bg=cyan] | "
-    tmux set-window-option -q window-status-current-format "#[fg=cyan, bg=cyan] | #[fg=white, bg=cyan] #I | ${PWD##/*/} #[fg=cyan, bg=cyan] | "
-  fi
-}
 
-tmux set -g status-right '#[fg=green][#[fg=white]#(echo $USER)@#H#{pane_current_path}#[fg=green]][#[fg=black,bold]%d-%h-%Y #[bg=green,fg=white,bold]%H:%M:%S#[default]#[fg=green]]'
-
-printf '\033%s\007' $(basename $1) # show full path use $1 instead of $(basename $1)
+printf '\033%s\007' $(echo $USER'@') # show full path use $1 instead of $(basename $1)
