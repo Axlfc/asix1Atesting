68a064d5c5af87d0869503f3adca4b8407d0bf68
axel <axelfernandezcurros@gmail.com>
2020-09-30 13:27:03 +0200

    changes

---
+++ asciitobin.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys
+import binascii
+
+def bta(asciitext):
+    try:
+        asciitext = str(sys.argv)
+        print((bin(int.from_bytes(sys.argv[1].encode(), 'big')))[2:])
+    except ValueError:
+        print("ERROR: asciitobin needs ASCII characters")
+        exit(1)
+bta(sys.argv[1])
