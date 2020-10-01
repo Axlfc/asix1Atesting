5387a9edb9e4a3d719f2511df51c51208718d3dd
axel <axelfernandezcurros@gmail.com>
2020-10-01 15:35:10 +0200

    changes

--- aliases/.tmux_path.sh
+++ aliases/.tmux_path.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #must make this file executable 'chmod +x ~/.tmux_path.sh'
 
-printf '\033%s\007' $(echo $USER'@') # show full path use $1 instead of $(basename $1)
+printf '\033%s\007' $(echo $USER) # show full path use $1 instead of $(basename $1)
